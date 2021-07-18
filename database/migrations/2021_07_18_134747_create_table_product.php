c88891523c99af6a8395290274b3325eb6b51a7f
rezam28 <rezamaulana.email@gmail.com>
2021-07-19 01:30:15 +0700

    first commit

---
+++ database/migrations/2021_07_18_134747_create_table_product.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableProduct extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('table_product', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->integer('price');
+            $table->integer('quantity');
+            $table->text('description');
+            $table->boolean('acive')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table_product');
+    }
+}
